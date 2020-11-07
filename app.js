056daea5167ce902bea98bb2b999d74b0af124ea
JohnCS94 <JohnScipion@yahoo.com>
2020-11-07 12:15:14 -0500

    made process port

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
 
@@ -36,6 +37,6 @@ app.post("/api/compose", (req, res) => {
     console.log(req.body.post);
 })
 
-app.listen(8080, function() {
-  console.log("Server started on port 3000");
+app.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}`);
 });
